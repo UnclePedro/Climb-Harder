46a664447784b37051c680a951ca43f021b3582f
Uncle Pedro <peterforsyth10@gmail.com>
2024-06-30 13:17:29 +1000

    Save SeasonNotes to state

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,14 @@ import Home from "./components/Home.tsx";
 import SeasonNotes from "./components/SeasonNotes.tsx";
 
 function App() {
-  const [trainingFocuses, setTrainingFocuses] = useState("Flexibility");
-  const [goals, setGoals] = useState("Redpoint LSD");
-  const [achievements, setAchievements] = useState(
-    "Redpointed In Between Dreams"
-  );
-  let [displaySeasonNotes, setDisplaySeasonNotes] = useState(false);
-
-  const toggleDisplaySeasonNotes = () => {
-    if (!displaySeasonNotes) {
-      setDisplaySeasonNotes(true);
-    } else setDisplaySeasonNotes(false);
-  };
+  const [displaySeasonNotes, setDisplaySeasonNotes] = useState(false);
 
   return (
     <>
       {displaySeasonNotes ? (
-        <SeasonNotes
-          trainingFocuses={trainingFocuses}
-          goals={goals}
-          achievements={achievements}
-          seasonNotesClose={toggleDisplaySeasonNotes}
-        />
+        <SeasonNotes onClose={() => setDisplaySeasonNotes(false)} />
       ) : (
-        <Home seasonNotesOpen={toggleDisplaySeasonNotes} />
+        <Home seasonNotesOpen={() => setDisplaySeasonNotes(true)} />
       )}
     </>
   );
--- src/components/SeasonNotes.tsx
+++ src/components/SeasonNotes.tsx
@@ -1,28 +1,40 @@
-const SeasonNotes = (props: {
-  trainingFocuses: string;
-  goals: string;
-  achievements: string;
-  seasonNotesClose: any;
-}) => {
+import { useState } from "react";
+
+interface Props {
+  onClose: () => void;
+}
+
+const SeasonNotes = ({ onClose }: Props) => {
+  const [trainingFocuses, setTrainingFocuses] = useState("Flexibility");
+  const [goals, setGoals] = useState("Redpoint LSD");
+  const [achievements, setAchievements] = useState(
+    "Redpointed In Between Dreams"
+  );
   return (
     <>
       <div className="p-6">
         <p className="font-bold pb-2 text-left">Training Focuses</p>
-        <textarea className="w-full h-48 border border-gray-300 rounded resize-y">
-          {props.trainingFocuses}
+        <textarea
+          onBlur={(element) => setTrainingFocuses(element.target.value)}
+          className="w-full h-48 border border-gray-300 rounded resize-y"
+        >
+          {trainingFocuses}
         </textarea>
         <p className="font-bold pb-2 text-left">Goals</p>
-        <textarea className="w-full h-48 border border-gray-300 rounded resize-y">
-          {props.goals}
+        <textarea
+          onBlur={(element) => setGoals(element.target.value)}
+          className="w-full h-48 border border-gray-300 rounded resize-y"
+        >
+          {goals}
         </textarea>
         <p className="font-bold pb-2 text-left">Achievements</p>
-        <textarea className="w-full h-48 border border-gray-300 rounded resize-y">
-          {props.achievements}
-        </textarea>
-        <button
-          className="bg-blue-200 rounded-lg p-2"
-          onClick={props.seasonNotesClose}
+        <textarea
+          onBlur={(element) => setAchievements(element.target.value)}
+          className="w-full h-48 border border-gray-300 rounded resize-y"
         >
+          {achievements}
+        </textarea>
+        <button className="bg-blue-200 rounded-lg p-2" onClick={onClose}>
           Close
         </button>
       </div>
